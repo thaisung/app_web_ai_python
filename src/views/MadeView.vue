185d1878f082d16373d5b0b1c90d3efeb0ed7631
thaisung <vuthaind@gmail.com>
2024-01-04 11:58:12 +0700

    hhsahdgjajsdhjasd

--- src/views/MadeView.vue
+++ src/views/MadeView.vue
@@ -82,6 +82,43 @@
           </div>
         </router-link>
       </div>
+      <div class="flex justify-center gap-2 mt-4 w-full">
+        <a
+          v-show="counter.san_pham_danh_sach.previous"
+          v-on:click="counter.product_page(counter.san_pham_danh_sach.previous)"
+          class="flex justify-center items-center bg-white hover:bg-gray-100 px-3 py-2 rounded-md shadow-md cursor-pointer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20"
+            viewBox="0 0 320 512"
+          >
+            <path
+              d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
+            />
+          </svg>
+        </a>
+        <div
+          class="flex justify-center items-center bg-white px-3 py-2 rounded-md shadow-md text-[18px] font-bold"
+        >
+          {{ counter.san_pham_danh_sach.current_page }}
+        </div>
+        <a
+          v-show="counter.san_pham_danh_sach.next"
+          v-on:click="counter.product_page(counter.san_pham_danh_sach.next)"
+          class="flex justify-center items-center bg-white px-3 py-2 hover:bg-gray-100 rounded-md shadow-md cursor-pointer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20"
+            viewBox="0 0 320 512"
+          >
+            <path
+              d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"
+            />
+          </svg>
+        </a>
+      </div>
       <p class="py-4 font-bold text-[20px]">Sản phẩm gợi ý</p>
       <div class="grid grid-cols-5 gap-4 w-full">
         <router-link
